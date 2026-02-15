cee19b7dc97f846fa2e0b5d0d61b96f851d00d59
Stef4anny <fannyg.lopes@gmail.com>
2026-02-15 17:46:01 -0300

    feat: adiciona interface interativa do jogo com HTML e CSS e JS

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Olá, mundo!!")
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        // Estrutura de decisão
+        if (this.tipo === "mago") {
+            ataque = "magia";
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada";
+        } else if (this.tipo === "monge") {
+            ataque = "artes marciais";
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken";
+        } else {
+            ataque = "um ataque desconhecido";
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Criando objetos (heróis)
+
+let heroi1 = new Heroi("Arthus", 30, "guerreiro");
+let heroi2 = new Heroi("Merlin", 150, "mago");
+let heroi3 = new Heroi("Lee", 40, "monge");
+let heroi4 = new Heroi("Ryu", 25, "ninja");
+
+// Chamando o método atacar
+
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
